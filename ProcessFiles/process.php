c75dcfce638ccd243ab25f8f6e0f4b8a68ccd4fe
Carlos Calderon <calderoncarloos@gmail.com>
2019-04-07 18:07:27 -0400

    Process Added

--- ProcessFiles/process.php
+++ ProcessFiles/process.php
@@ -1,1 +1,14 @@
 <?php
+
+$mysqli = new mysqli('127.0.0.1:53694', 'localdb', 'azure', '6#vWHD_$') or die(mysqli_error($mysqli));
+
+if (isset($_POST['save'])) {
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$location = $_POST['location'];
+	$skills = $_POST['skills'];
+	$password = $_POST['password'];
+
+
+	$mysqli->query("INSERT INTO users (Name, Email, Location, Skills, Password) VALUES ('$name', '$email'. '$location', '$skills', '$password')") or die(mysql_error($mysqli));
+}
