7a02271c238442fc0b3943d781782bafad661b8f
Carlos Calderon <calderoncarloos@gmail.com>
2019-04-07 16:14:53 -0400

    Boostrap Added

--- index.php
+++ index.php
@@ -2,16 +2,27 @@
 
 ?>
 
-<div class="body-content">
-  <div class="module">
-    <h1>Create an user</h1>
-    <form class="form" action="form.php" method="post" enctype="multipart/form-data" autocomplete="off">
-      <div class="alert alert-error"><?= $_SESSION['message'] ?></div>
-      <input type="text" placeholder="Name" name="name" required />
-      <input type="email" placeholder="Email" name="email" required />
-      <input type="text" placeholder="Location" name="location" required />
-      <input type="text" placeholder="Skills" name="skills" required />
-      <input type="submit" value="Register" name="register" class="btn btn-block btn-primary" />
-    </form>
+<link rel="stylesheet" type="text/css" href="style/index.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<div class="body-content"> <!-- Web Design -->
+  <div> <!-- Navigation Bar -->
+    <nav class="navbar navbar-expand-sm bg-light">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 1</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 2</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 3</a>
+    </li>
+  </ul>
+</nav>
   </div>
 </div>
+
+<!-- Boostrap 4 JavaScript Connections -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
