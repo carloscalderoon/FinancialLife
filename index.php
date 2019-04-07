43f28e8045bd13c1b6c7a8177baa0610ed2b75cc
Carlos Calderon <calderoncarloos@gmail.com>
2019-04-07 17:01:05 -0400

    Boostrap Fixed

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
   <title>Project</title>
   <link rel="stylesheet" type="text/css" href="style/index.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
 <body>
     <!-- Initial Form -->
@@ -22,7 +24,7 @@
     </div>
           <div class="form-group">
       <label>Password</label>
-      <input type="password" name="password" value="Enter a Password">
+      <input type="password" name="password">
     </div>
           <div class="form-group">
       <label>Location</label>
