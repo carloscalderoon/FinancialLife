1204da2755b0fb8ed86d9bfec4db08c280ec2104
Carlos Calderon <calderoncarloos@gmail.com>
2019-04-07 16:56:58 -0400

    New Index

--- index.php
+++ index.php
@@ -1,28 +1,48 @@
+<!DOCTYPE HTML>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Project</title>
+  <link rel="stylesheet" type="text/css" href="style/index.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+<body>
+    <!-- Initial Form -->
+    <div class="row justify-content-center"></div>
+    <form action="" method="POST">
+      <div class="form-group">
+      <label>Name</label>
+      <input type="text" name="name" value="Enter your Name">        
+      </div>
+      <div class="form-group">
+      <label>Email</label>
+      <input type="email" name="email" value="Enter your Email">
+    </div>
+          <div class="form-group">
+      <label>Password</label>
+      <input type="password" name="password" value="Enter a Password">
+    </div>
+          <div class="form-group">
+      <label>Location</label>
+      <input type="text" name="location" value="Enter your Location">
+    </div>
+          <div class="form-group">
+      <label>Skills</label>
+      <input type="text" name="skills" value="Enter your Skills">
+    </div>
+      <button type="submit" name="save">Save</button>
+    </form>
+  </div>
+</body>
+</html>
+
 <?php
 
 ?>
 
-<link rel="stylesheet" type="text/css" href="style/index.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<div class="body-content"> <!-- Web Design -->
-  <div> <!-- Navigation Bar -->
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" href="#">Link 1</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Link 2</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Link 3</a>
-    </li>
-  </ul>
-</nav>
-  </div>
-</div>
 
-<!-- Boostrap 4 JavaScript Connections -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+
