9c0912a24286187bb1726e661d55216a2f5f0530
Carlos Calderon <calderoncarloos@gmail.com>
2019-04-09 23:15:22 -0400

    Database?

--- footer.php
+++ footer.php
@@ -5,6 +5,15 @@
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script></head>
+    <!-- Jquery JS-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <!-- Vendor JS-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <script src="vendor/datepicker/moment.min.js"></script>
+    <script src="vendor/datepicker/daterangepicker.js"></script>
+
+    <!-- Main JS-->
+    <script src="js/global.js"></script>
 
 </body>
 
--- index.php
+++ index.php
@@ -4,35 +4,6 @@ $page_title = 'Login';
 require_once('header.php'); ?>
 
 
-<!-- Initial Form -->
-    <div class="row justify-content-center">
-    <form action="process.php" method="post" >
-      <div class="form-group">
-        <label>First Name</label>
-        <input type="text" name="firstname" class="form-control">        
-      </div>
-      <div class="form-group">
-        <label>Last Name</label>
-        <input type="text" name="lastname" class="form-control">        
-      </div>
-      <div class="form-group">
-        <label>Email</label>
-        <input type="email" name="email" class="form-control">
-      </div>
-      <div class="form-group">
-        <label>Password</label>
-        <input type="password" name="password" class="form-control">
-      </div>
-      <div class="form-group">
-        <label>Location</label>
-        <input type="text" name="location" class="form-control">
-      </div>
-      <div class="form-group">
-        <label>Skills</label>
-        <input type="text" name="skills" class="form-control">
-      </div>
-      <button class="btn btn-primary" type="submit" value="Save">Save</button>
-    </form>
-  </div>
+<?php require_once('registration.php') ?>
 
   <?php require_once('footer.php'); ?>
