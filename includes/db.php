04728bd0d9f1b8595c6a2045ecd27c5dd092b749
Carlos Calderon <calderoncarloos@gmail.com>
2019-04-09 21:20:47 -0400

    Database Works!!

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,10 @@
-<?php 
-
-	$conn = new PDO('mysql:127.0.0.1:53694')
-
+<?php
+try {
+  //Connection to Database
+  $db = new PDO('mysql:host=127.0.0.1:53694; dbname=localdb', 'azure','6#vWHD_$');
+  echo "<p> You are connected! Yay! </p>";
+} catch (PDOException $e) {
+    echo "<p> Sorry you can't connect.</p>";
+    echo $e;
+}
 ?>
--- process.php
+++ process.php
@@ -1,14 +1,4 @@
 <?php
-
-try {
-	//Connection to Database
-	$db = new PDO('mysql:host=127.0.0.1:53694; dbname=localdb', 'azure','6#vWHD_$');
-	echo "<p> You are connected! Yay! </p>";
-} catch (PDOException $e) {
-		echo "<p> Sorry you can't connect.</p>";
-		echo $e;
-}
-
 if (isset($_POST['save'])) {
 	$firstname = $_POST['firstname'];
 	$lastname = $_POST['lastname'];
@@ -20,3 +10,4 @@ if (isset($_POST['save'])) {
 
 	$mysqli->query("INSERT INTO users (FirstName, LastName, Email, Location, Skills, Password) VALUES (':firstname', ':lastname', 'email'. '$location', '$skills', '$password')") or die(mysql->error);
 }
+?>
