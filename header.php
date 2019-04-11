df28867042078b47fa498c1807f72b1b26b3ea0f
Carlos Calderon <calderoncarloos@gmail.com>
2019-04-10 20:16:14 -0400

    Inicio

--- header.php
+++ header.php
@@ -27,8 +27,8 @@
 
 <body id="page-top">
       <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNavFixed">
-    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-dark fixed-top "id="mainNavFixed">
+        <div class="container">
       <a class="navbar-brand js-scroll-trigger" href="index.php">iFinance</a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
